4e17f2f62f660da12f8728b7dc40cae567d17fef
MJ <michaljalowik@gmail.com>
2020-12-23 23:01:01 +0100

    205

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, NavLink } from 'react-router-dom';
+import { Route, NavLink, Switch } from 'react-router-dom';
 
 import Posts from './Posts/Posts'
 import './Blog.css';
@@ -8,33 +8,20 @@ import NewPost from './NewPost/NewPost'
 import FullPost from '../../containers/Blog/FullPost/FullPost'
 
 class Blog extends Component {
-
-
-
-
-
-
-
-
     render() {
-
-
-
-
-
         return (
             <div className="Blog">
                 <header>
                     <nav>
                         <ul>
-                            <li><NavLink 
-                            to="/" 
-                            exact
-                            activeClassName="my-active" 
-                            activeStyle={{
-                                color: '#fa923f',
-                                textDecoration: 'underline'
-                            }}>Home</NavLink></li>
+                            <li><NavLink
+                                to="/"
+                                exact
+                                activeClassName="my-active"
+                                activeStyle={{
+                                    color: '#fa923f',
+                                    textDecoration: 'underline'
+                                }}>Home</NavLink></li>
                             <li><NavLink to={{
                                 pathname: '/new-post',
                                 hash: '#submit',
@@ -46,9 +33,14 @@ class Blog extends Component {
                 </header>
                 { /*<Route path="/" exact render={() => <h1>Home</h1>}></Route>
                 <Route path="/" render={() => <h1>Home 2</h1>}></Route>*/}
+
                 <Route path="/" exact component={Posts}></Route>
-                <Route path="/:id" exact component={FullPost}></Route>
-                <Route path="/new-post" component={NewPost}></Route>
+                <Switch>
+                    <Route path="/new-post" component={NewPost}></Route>
+                    <Route path="/:id" exact component={FullPost}></Route>
+
+                </Switch>
+
 
             </div>
         );
--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -41,7 +41,6 @@ class Posts extends Component {
                 return (
                     <Link to={'/' + post.id} key={post.id}>
                         <Post
-                        
                             title={post.title}
                             author={post.author}
                             clicked={() => this.postSelectedHandler(post.id)}
